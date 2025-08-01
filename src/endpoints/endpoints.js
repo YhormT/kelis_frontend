e982c2cbbec5f52658377c7522df4abc35e94a7f
Niidaddey2025 <sdardey1221@gmail.com>
2025-08-01 16:58:09 +0000

    base_url updated to railway hosted domain link

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,5 +1,7 @@
 //const BASE_URL = 'https://datapackage-production.up.railway.app';
 
-const BASE_URL = 'http://localhost:5000';
+//const BASE_URL = 'http://localhost:5000';
+
+const BASE_URL = 'https://kelisbackend-production.up.railway.app';
 
 export default BASE_URL;
